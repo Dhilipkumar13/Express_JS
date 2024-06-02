ffe2c75339fb4d84c0fbdba752038f623f967a21
DHILIPKUMAR M <139251135+Dhilipkumar13@users.noreply.github.com>
2024-06-02 17:06:01 +0530

    Update and rename api to root.js

---
+++ REST API/router/root.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Route for index.html with optional .html extension
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'src', 'view', 'index.html'));
+});
+
+module.exports = router; // Ensure the router is exported
