3b5b70b087768f6dccdfa2ebc55c1020a86b5375
DHILIPKUMAR M <139251135+Dhilipkumar13@users.noreply.github.com>
2024-06-02 17:06:36 +0530

    Update user.js

--- REST API/router/api/user.js
+++ REST API/router/api/user.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const router = express.Router();
+const userController = require('../../Controller/userController');
+router.route('/')
+    .get(userController.userGet)
+    .post(userController.userPost)
+    .put(userController.userPut)
+    .delete(userController.userDelete);
+
+router.route('/:id')
+    .get(userController.userGetId);
+
+module.exports = router; // Ensure the router is exported
